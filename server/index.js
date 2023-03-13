0194dc87e574484a6dc77441133eb5c90f054a79
amschel99 <6290.2020@students.ku.ac.ke>
2023-03-13 15:36:04 +0300

    basic express server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
-//server
+const express= require("express")
+const dotenv= require("dotenv")
+const path= require("path")
+const cors = require('cors')
+dotenv.config()
+const app= express ()
+app.get("/", (req, res) => {
+  
+    res.sendFile(path.join(__dirname, "../client", "index.html"));
+
+    console.log(`hey got called`)
+  });
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join(__dirname, "client")));
+const PORT= process.env.PORT || 8800
+app.listen(PORT,()=>{
+console.log(`app listening on ${PORT}`)
+})
