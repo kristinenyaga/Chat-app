009cd88aa64147056c401b32a104adb96a095964
kibzrael <kibraphael7@gmail.com>
2023-03-16 11:28:18 +0300

    GET requests

--- server/controllers/getComments.ts
+++ server/controllers/getComments.ts
@@ -1,10 +1,16 @@
+import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
-export const getComments=async (req:Request,res:Response)=>{
-    try{
-const comments= null //TODO get comments from the db
-res.json(comments)
-    }
-    catch(e:any){
-res.json(e.message)
-    }
-}
+
+const prisma = new PrismaClient();
+
+export const getComments = async (req: Request, res: Response) => {
+  try {
+    const postId = parseInt(req.params.id);
+    const comments = await prisma.comment.findMany({
+      where: { postId: postId },
+    });
+    res.json(comments);
+  } catch (e: any) {
+    res.json(e.message);
+  }
+};
--- server/routes/post.ts
+++ server/routes/post.ts
@@ -6,14 +6,12 @@ import { deleteComment } from "../controllers/deleteComment";
 import { deleteLike } from "../controllers/deleteLike";
 import { deletePost } from "../controllers/deletePost";
 import { getComments } from "../controllers/getComments";
-import { getNumberOfLikes } from "../controllers/getNumberOfLikes";
 import { getPosts } from "../controllers/getPosts";
 export const routerPost = express.Router();
-routerPost.route("/").get(getPosts)
+routerPost.route("").get(getPosts);
 routerPost.route("/create").post(createPost);
 routerPost.route("/:id").delete(deletePost);
 routerPost.route("/:id/likes").post(createLike);
-routerPost.route("/:id/likes/number").get(getNumberOfLikes);
 routerPost.route("/:id/likes").delete(deleteLike);
 routerPost.route("/:id/comments").post(createComment).get(getComments);
 routerPost.route("/:id/comments/:cid").delete(deleteComment);
